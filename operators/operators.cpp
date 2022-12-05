42095cdfe3c4659b04f45c2e41c0b919aea23e8c
Thiago <thiagomacenogoulart@gmail.com>
2022-12-04 21:10:16 -0300

    master

---
+++ operators/operators.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+int main() {
+    // Arithmetic operators = return the result of a specific
+    //                        arithmetic operation (+ - * /)
+
+    // parenthesis()
+    // multiplication & division * /
+    // addition & subtraction + -
+
+    int students = 20;
+
+    //students = students + 1;
+    //students += 1;
+    //students++;
+
+    //students = students - 1;
+    //students -= 1;
+    //students--;
+
+    //students = students * 1;
+    //students *= 1;
+
+    //students = students / 1;
+    //students /= 1;
+
+    int remainder = students % 3;
+
+    std::cout << remainder <<'\n';
+    std::cout << students <<'\n';
+
+    return 0;
+}

3b8e22318e8a55a299e24909b007340766ef9bd6
Thiago <thiagomacenogoulart@gmail.com>
2022-12-04 21:57:16 -0300

    master

---
+++ typeconversion/typeconversion.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main() {
+    // type conversion = conversion a value of one data type to another
+    //                   Implicit = automatic
+    //                   Explicit = Precede value with new data type (int)
+
+    //int x = (int) 3.14; // Explicit
+    //char x = 100;
+
+    //std::cout << x; 
+    //std::cout << (char) 100;
+
+    int correct = 8;
+    int questions = 10;
+    double score = correct/(double)questions * 100;
+
+    std::cout << score << "%";
+
+
+    return 0;
+}

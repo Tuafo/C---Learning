c2af1f39439fbbb1aa2ecff103f64bd5aa000bec
Thiago <thiagomacenogoulart@gmail.com>
2022-12-05 16:23:28 -0300

    master

---
+++ useful math related functions/usefulMathRelatedFuncions.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath> //www.cplusplus.com/reference/cmath/
+
+int main()
+{
+    double x = 3.99;
+    double y = 4;
+    double z;
+
+    //z = std::max(x, y);
+    //z = std::min(x, y);
+    //z = pow(2, 3); //function to get the power(potência) of a number,
+    //z = sqrt(9); // The sqrt function returns the square(raiz quadrada) root of x
+    //z = abs(-3); // returns the absolute value of x
+    //z = round(x); // rounds the value 
+    //z = ceil(x); // rounds the value up
+    //z = floor(x); // rounds the value down
+
+    std::cout << z;
+    
+    return 0;
+}

bfbcec3f9432c24923ffebea4aecb602d7f0b821
Thiago <thiagomacenogoulart@gmail.com>
2022-12-06 17:08:36 -0300

    master

---
+++ forLoops/forLoops.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+    for (int index = 1; index <= 10; index++)
+    {
+        std::cout << index << '\n';
+    }
+    
+    std::cout << "HAPPY NEW YEAR!\n";
+
+    return 0;
+}
+

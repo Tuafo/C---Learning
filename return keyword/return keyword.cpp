126c6fbb0ebaf04cde4d77dadfe453ef5a3a22dc
Thiago <thiagomacenogoulart@gmail.com>
2022-12-06 22:32:23 -0300

    master

---
+++ return keyword/return keyword.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+double square(double length);
+double cube(double length);
+
+std::string concatStrings(std::string string1, std::string string2);
+
+int main()
+{
+    // return = return a value back to the spot
+    //          where you called the encompassing function
+
+    double length = 6.0;
+    double area = square(length);
+    double volume = cube(length);
+
+    std::cout << "Area: " << area << "cm^2\n";
+    std::cout << "Volume: " << volume << "cm^3\n";
+
+
+    std::string firstName = "Thiago";
+    std::string lastName = "Goulart";
+    std::string fullName = concatStrings(firstName, lastName);
+
+    std::cout << fullName;
+
+    return 0;
+}
+
+double square(double length)
+{
+    return length * length;
+}
+
+double cube(double length)
+{
+    return length * length * length;
+}
+
+std::string concatStrings(std::string string1, std::string string2)
+{
+    return string1 + " " + string2;
+}
